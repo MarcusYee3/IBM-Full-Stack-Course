b1c1fdfdc27f69542c7d306edf038537c9e9ee72
Marcus Yee <marcusy3@illinois.edu>
2026-03-16 21:46:41 -0400

    initial commit

--- calculateArea/calculateArea.js
+++ calculateArea/calculateArea.js
@@ -1,9 +1,14 @@
 let length;
 let width;
+
 function calculateArea() {
+    // 1. Fixed the typo: getElementById (removed the extra 'e')
     length = parseFloat(document.getElementById("length").value);
-    width = parseFloat(document.getElemenetById("width").value);
+    width = parseFloat(document.getElementById("width").value);
 
     let area = length * width;
-    document.getElementById("result").innerText = "The area of the rectangle is ${area}";$
+
+    // 2. Fixed the string: Use backticks (`) instead of quotes ("") 
+    // to make the ${area} variable actually show up.
+    document.getElementById("result").innerText = `The area of the rectangle is ${area}`;
 }
